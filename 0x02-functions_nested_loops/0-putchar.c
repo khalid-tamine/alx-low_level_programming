6ca3bfe2e537b986e687e77cc46d2be25cb4d456
khalid-tamine <khalidtamine1997@gmail.com>
2023-03-21 09:49:40 -0700

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *main - print putchar
+ * main - print putchar
  * _putchar: print the alphabets putchar.
  *
  * Return: Always 0 (Success)
@@ -13,9 +13,9 @@ int main(void)
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
-        _putchar('h');
+	_putchar('h');
 	_putchar('a');
-        _putchar('r');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
