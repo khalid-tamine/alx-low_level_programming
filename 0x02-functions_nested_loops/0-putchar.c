12011de10615454fe2336b71f30cdb527d90e213
khalid-tamine <khalidtamine1997@gmail.com>
2023-03-21 09:48:09 -0700

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * main - Prints ALX as a message.
+ *main - print putchar
+ * _putchar: print the alphabets putchar.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+        _putchar('h');
+	_putchar('a');
+        _putchar('r');
 	_putchar('\n');
 	return (0);
 }
