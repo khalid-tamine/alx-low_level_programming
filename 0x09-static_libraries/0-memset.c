016222670ad68f254acaecefb1dd73d2250286f2
khalid-tamine <khalidtamine1997@gmail.com>
2023-04-08 07:44:21 -0700

    static library

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * *_memset - prints a string, followed by a new line.
+ * @s: string
+ * Return: no return.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	_putchar(*s);
+	_putchar(b);
+	_putchar(n);
+	return 0;
+}
