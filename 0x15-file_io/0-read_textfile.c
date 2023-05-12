fcaea50f5d76801319d39b0b8fb91281e0777267
khalid-tamine <khalidtamine1997@gmail.com>
2023-05-12 14:45:02 -0700

    0x15 fileIO

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,37 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
+ * read_textfile - Reads a text file and writes
+ * its contents to standard output.
+ * @filename: The name of the file to read.
+ * @letters: The maximum number of letters to read and write.
  *
- * Return: numbers of letters printed. It fails, returns 0.
+ * Return: The actual number of letters read and written, or 0 on failure.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
-	
+	ssize_t o, r, nw;
+	char *buffer;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
 
-	char buf[letters];
-	if (!buf)
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	nw = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || nw == -1 || nw != r)
+	{
+		free(buffer);
 		return (0);
+	}
 
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
+	free(buffer);
+	close(o);
 
-	close(fd);
-
-	free(buf);
-
-	return (nwr);
+	return (nw);
 }
